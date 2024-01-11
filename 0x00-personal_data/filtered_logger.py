6bde96de08ee6937c3fb6559e96341124536f66f
Mike-chege <mikechege93@gmail.com>
2024-01-11 10:27:45 +0000

    Using regex to replace occurences of certain field values

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -23,7 +23,9 @@ def filter_datum(fields: List[str], redaction: str,
 
 
 def get_logger() -> logging.Logger:
-    """ Returns logger obj  """
+    """
+    Returns logger obj
+    """
     logger = logging.getLogger('user_data')
     logger.setLevel(logging.INFO)
     logger.propagate = False
@@ -36,7 +38,8 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> connection.MySQLConnection:
     """
-    Connect to mysql server with environmental vars
+    Connect to mysql server
+    With environmental vars
     """
     username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
     password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
@@ -51,19 +54,25 @@ def get_db() -> connection.MySQLConnection:
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
+    """
+    Redacting Formatter class
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """ inits class instance """
+        """
+        Inits class instance
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """ filters values in incoming log records """
+        """
+        Filters values in incoming log records
+        """
         return filter_datum(
             self.fields, self.REDACTION, super(
                 RedactingFormatter, self).format(record),
@@ -73,7 +82,8 @@ class RedactingFormatter(logging.Formatter):
 def main() -> None:
     """
     Obtain a database connection using get_db
-    and retrieve all rows in the users table and display each row
+    And retrieve all rows in the users table
+    And display each row
     """
     db = get_db()
     cur = db.cursor()
